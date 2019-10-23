3c0ba48af78c6648d19f16567b144e68277b403b
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2019-10-23 17:08:10 +0530

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,7 +1,6 @@
-include wget
-wget::fetch {  "war":
-source  => ' http://ec2-18-224-155-110.us-east-2.compute.amazonaws.com:8081/nexus/service/local/repositories/devopstraining/content/Codekillers/dynamic_new-101.war',
-destination => '/home/ubuntu/apache-tomcat-8.5.47/webapps/dynamic/',
-timeout  => 5,
-verbose  => true,
+node default{
+  package{ 'git' :
+  name => 'git',
+  ensure => installed,
+  }
 }
