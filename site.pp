9cb9144197f3dc1dab4043fcd31f7da03f828d48
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2019-10-24 16:34:46 +0530

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,6 +1,6 @@
 include wget
 wget::fetch { "Downloading war from nexus":
-  source      => 'http://ec2-18-224-155-110.us-east-2.compute.amazonaws.com:8081/nexus/service/local/repositories/devopstraining/content/Codekillers/dynamic_new-119.war',
+  source      => 'http://ec2-18-224-155-110.us-east-2.compute.amazonaws.com:8081/nexus/service/local/repositories/devopstraining/content/Codekillers/dynamic_new-120.war',
   destination => '/home/padmin/apache-tomcat-9.0.27/webapps/',
   timeout     => 5,
   verbose     => true,
